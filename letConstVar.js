83d5b86ecd055eacfb6dbaf36e34abbe6768fb6b
Mark <markandrews2023@gmail.com>
2023-10-27 13:16:12 -0400

    adding file

---
+++ letConstVar.js
@@ -0,0 +1,14 @@
+//1.
+  const PI = 3.14;
+
+//2.
+ //The difference is that let is a block-scoped varilbe and var isn't. Another difference is that you can redeclare var multiple times you can't with let. 
+
+//3.
+ //Const once assigned cannot be reasigned. You can't redeclare const. Const is block-scoped.
+
+//4.
+ //Const once assigned cannot be reasigned unlike let.
+
+//5.
+ //Variables are declared first before the value is assigne.
